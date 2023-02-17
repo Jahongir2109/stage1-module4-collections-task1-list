3efbb4ac10a6e4369f7e5dd7ff4b2472ede67fc5
Jahongir2109 <jahongirachilov004@gmail.com>
2023-02-17 15:57:33 +0500

    fixed not all

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -5,5 +5,19 @@ import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+        LinkedList<Integer> linkedList=new LinkedList<>();
+        if(sourceList.size()>0&& sourceList.get(0)%2!=0){
+            for (int i = sourceList.size()-1; i >= 0; i--) {
+                if (sourceList.get(i)%2!=0){
+                    linkedList.add(sourceList.get(i));
+                }
+            }
+        }
+            for (int i=0;i<sourceList.size();i++){
+                if (sourceList.get(i)%2==0){
+                    linkedList.addLast(sourceList.get(i));
+                }
+            }
+        return linkedList;
     }
 }
--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -1,15 +1,20 @@
 package com.epam.mjc.collections.list;
 
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        List<Integer> numberList=new LinkedList<>();
+        for (int i = 0; i < sourceList.size(); i++) {
+            numberList.add(Integer.valueOf(sourceList.get(i)));
+        }
+
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        return Integer.valueOf(a);
     }
 }
